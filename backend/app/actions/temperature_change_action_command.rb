f1a457d7a0862b36ebe68c1e05126b43409882ff
Glab <geoffrey.labruyere@epitech.eu>
2022-11-11 23:57:31 +0100

    [ADD] rspec for temp change [FIX] files names 'temparature'

--- backend/app/actions/temperature_change_action_command.rb
+++ backend/app/actions/temperature_change_action_command.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class TemparatureChangeActionCommand
-  def initialize(options)
+class TemperatureChangeActionCommand
+  def initialize(options, mocked_response = nil)
     @action_id = options["action_id"]
     @latitude = options["latitude"]
     @longitude = options["longitude"]
-    @last_temp = options["last_temp"] || current_temp
+    @last_temp = options["last_temp"] || current_temp(mocked_response)
   end
 
   def to_h
@@ -13,11 +13,12 @@ class TemparatureChangeActionCommand
   end
 
   private
-    def current_temp
+    def current_temp(mocked_response = nil)
       url = "https://api.open-meteo.com/v1/forecast?latitude=#{@latitude}&longitude=#{@longitude}&current_weather=true"
-      weather_info = HTTParty.get(url)
-      weather_info["current_weather"]["temperature"].to_f
+      weather_info = mocked_response || HTTParty.get(url)
+      return weather_info["current_weather"]["temperature"]
     rescue NoMethodError
       puts "Error: OpenMeteo return null"
+      return nil
     end
 end
---
+++ backend/app/actions/temperature_change_action_command_handler.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class TemperatureChangeActionCommandHandler
+  def initialize
+  end
+
+  def call(attributes, mocked_response = nil)
+    puts "Temperature Change Command Handler"
+
+    begin
+      url = "https://api.open-meteo.com/v1/forecast?latitude=#{attributes[:latitude]}&longitude=#{attributes[:longitude]}&current_weather=true"
+      weather_info = mocked_response || HTTParty.get(url)
+      current_temp = weather_info["current_weather"]["temperature"].to_f
+    rescue NoMethodError
+      puts "Error: OpenMeteo return null"
+      return false
+    end
+
+    last_temp = attributes[:last_temp].to_f
+    result = current_temp != last_temp
+
+
+    if result
+      puts "Temperature Change: #{result}"
+      action = Action.find(attributes[:action_id])
+      action.options["last_temp"] = current_temp.to_s
+      action.save
+    end
+
+    result
+  end
+end
