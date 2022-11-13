b9df5b9380a398d3b507576726735c5b39fed061
roy-corentin <corentin.roy@epitech.eu>
2022-11-13 18:54:14 +0100

    [FIX] fix behavior of discover movie command handler spec

--- backend/app/reactions/discover_movie_reaction_command_handler.rb
+++ backend/app/reactions/discover_movie_reaction_command_handler.rb
@@ -7,8 +7,19 @@ class DiscoverMovieReactionCommandHandler
   def call(attributes, gmail = nil, mocked_response = nil)
     puts "Discover Movie Command Handler" unless Rails.env.test?
 
+    unless mocked_response
+      begin
+        infos = HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_DB_KEY"]}&language=#{attributes[:language]}&region=#{attributes[:region]}&sort_by=vote_average.desc&include_adult=#{attributes[:adult]}&include_video=true&page=1&release_date.gte=#{attributes[:min_date]}&vote_count.gte=50&vote_average.gte=#{attributes[:min_score]}&without_genres=documentary&watch_region=FR")
+        nbPage = infos["total_pages"].to_i
+        nbPage = nbPage >= 500 ? 499 : nbPage
+      rescue NoMethodError
+        puts "Error: The Movie DB return null"
+        return false
+      end
+    end
+
     begin
-      movies = mocked_response || HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_DB_KEY"]}&language=#{attributes[:language]}&region=#{attributes[:region]}&sort_by=vote_average.desc&include_adult=#{attributes[:adult]}&include_video=true&page=1&release_date.gte=#{attributes[:min_date]}&vote_count.gte=50&vote_average.gte=#{attributes[:min_score]}&without_genres=documentary&watch_region=FR")
+      movies = mocked_response || HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["MOVIE_DB_KEY"]}&language=#{attributes[:language]}&region=#{attributes[:region]}&sort_by=vote_average.desc&include_adult=#{attributes[:adult]}&include_video=true&page=#{rand(1..nbPage)}&release_date.gte=#{attributes[:min_date]}&vote_count.gte=50&vote_average.gte=#{attributes[:min_score]}&without_genres=documentary&watch_region=FR")
       movie = movies["results"][rand(0..movies["results"].length-1)]
       date = DateTime.now.strftime("%d/%m/%Y")
     rescue Exception
